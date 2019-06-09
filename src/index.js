c25b8f630f8f6de99e7a388f5477497f8fc8c38b
Greg Jordan <gregljordan@gmail.com>
2019-06-09 03:27:13 -0400

    Setting up Latitude and error handling

--- src/index.js
+++ src/index.js
@@ -9,26 +9,27 @@ class App extends React.Component {
         // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO this.state!!!
         // constructor is used to initialize state
 
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 // in order to update the state object we called setState.
                 this.setState({lat: position.coords.latitude });
             },
-            err =>console.log(err)
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
         );
-
     }
-    
-   
-   
     // React says we have to define render!!!
     render () {
-        
-
-    return <div><h1>Latitude: {this.state.lat}</h1></div>
-    
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        } 
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>latitude: {this.state.lat}</div>
+        } 
+        return <div>Loading, please be patient.</div> 
     }
 }
 
